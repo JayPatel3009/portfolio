6919f06ee42510b284a7325df666d0ef06519655
30029533 <patel.jay3009@gmail.com>
2025-02-28 16:40:54 +1300

    Update footer links, refactor ProjectCard, revise data
    - Updated footer links in `footer.jsx` to correct GitHub URLs.
    - Refactored `project-card.jsx`:
      - Changed import statements to include `Link` from `next/link` and icons from `react-icons/ri`.
      - Converted `ProjectCard` function to an arrow function.
      - Added new `Link` components for "Code" and "Live" buttons with styling and icons.
    - Revised `description` field in `personal-data.js` for a more polished profile summary.
    - Added `code` and `demo` URLs for "Movies and TV Shows Web App" in `projects-data.js`.

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -17,7 +17,7 @@ function Footer() {
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/JayPatel3009/my-portfolio"
+              href="https://github.com/JayPatel3009/portfolio"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <IoStar />
@@ -25,7 +25,7 @@ function Footer() {
             </Link>
             <Link
               target="_blank"
-              href="https://github.com/JayPatel3009/my-portfolio/fork"
+              href="https://github.com/JayPatel3009/portfolio/fork"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <CgGitFork />
